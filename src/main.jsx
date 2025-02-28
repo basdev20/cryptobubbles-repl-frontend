63338ffd3104e76ef50dc536cad09ab08e9684d5
hichamDe <hicham.iamiri200@gmail.com>
2025-02-28 17:30:38 +0000

    the chart is added in the chart.jsx and shadcn is successfully installed

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Chart from './home_page/chart.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <App />
+    <Chart />
   // </StrictMode>,
 )
